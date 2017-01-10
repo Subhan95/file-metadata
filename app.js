95999a6867a2e15e74342b421677b083edc54ec2
subhan <khansubhan20@yahoo.com>
2017-01-10 18:00:33 +0530

    Edited app.js for uploading files

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 var express = require('express');
-var multer = require('multer');
+var multer = require('multer'); 
+// Multer works only on forms whose enctype="multipart/form-data"
 var path = require('path');
-var upload = multer({ dest: 'uploads/' })
+var upload = multer({ storage: multer.memoryStorage() })
 
 var app = express()
 
 app.use(express.static(path.join(__dirname,'public')))
 
-// app.get('/', function(req, res){
-
-// })
-
 app.post('/upload', upload.single('file'),function(req, res){
-	fs.unlink()
-	if (req.file) {
-		res.json({size: req.file.size})
-	}
+	res.json({name: req.file.originalname, size: req.file.size})
 })
 
-// process.env.PORT
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
 console.log('Server running on port 3000');
